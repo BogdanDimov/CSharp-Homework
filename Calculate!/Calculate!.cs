4f5e7933941a953194c00c8db2a6fd1c0143f358
BogdanDimov <bogdan.dimov@outlook.com>
2016-10-28 23:08:30 +0300

    Add new projects

---
+++ Calculate!/Calculate!.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculate_
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sum = 1.0, x;
+            ulong factorial = 1, N;
+
+            N = Convert.ToUInt64(Console.ReadLine());
+            x = Convert.ToDouble(Console.ReadLine());
+
+            for (ulong i = 1; i <= N; i++)
+            {
+                factorial *= i;
+                sum += factorial / Math.Pow(x, i);
+            }
+
+            Console.WriteLine("{0:f5}", sum);
+        }
+    }
+}
